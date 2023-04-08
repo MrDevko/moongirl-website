d88e64c86cb2c835ff80f29a1b82a9b3f00b0b12
Okved <devko.dev@yandex.com>
2023-04-09 01:29:43 +0200

    fix sound

---
+++ sound.js
@@ -0,0 +1,61 @@
+function soundisOff() {
+  try {
+    document.getElementById("soundONButton").classList.add("d-none");
+    document.getElementById("soundONButton").classList.remove("d-block");
+    document.getElementById("soundOFFButton").classList.add("d-block");
+    document.getElementById("soundOFFButton").classList.remove("d-none");
+  } catch (error) {
+  }
+}
+
+function soundisOn() {
+  try {
+    document.getElementById("soundOFFButton").classList.add("d-none");
+    document.getElementById("soundOFFButton").classList.remove("d-block");
+    document.getElementById("soundONButton").classList.add("d-block");
+    document.getElementById("soundONButton").classList.remove("d-none");
+  } catch (error) {
+  }
+}
+
+function checkSound() {
+  if (localStorage.getItem("muted").toString() == "true") {
+    soundisOff();
+  } else {
+    soundisOn();
+
+    beat = new Audio(beatUrl);
+    beat.play().catch((e) => {
+      window.addEventListener(
+        "click",
+        () => {
+          beat.play();
+        },
+        { once: true }
+      );
+    });
+  }
+}
+
+function turnOff() {
+  try {
+    beat.pause();
+  } catch (error) {
+  }
+  localStorage.setItem("muted", true);
+  soundisOff();
+}
+
+function turnOn() {
+  try {
+    beat = new Audio(beatUrl);
+    beat.play();
+  } catch (error) {
+  }
+  localStorage.setItem("muted", false);
+  soundisOn();
+}
+
+window.addEventListener("load", (event) => {
+  checkSound();
+});
