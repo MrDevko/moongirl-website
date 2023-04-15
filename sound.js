abba53e75d5fba2100b1985e62ed3aa6af8273b2
Okved <devko.dev@yandex.com>
2023-04-15 16:19:46 +0200

    Update sound.js

--- sound.js
+++ sound.js
@@ -5,7 +5,7 @@ function soundisOff() {
     document.getElementById("soundOFFButton").classList.add("d-block");
     document.getElementById("soundOFFButton").classList.remove("d-none");
   } catch (error) {
-    console.log("soundisOff function",error)
+    console.log("soundisOff function", error);
   }
 }
 
@@ -16,50 +16,55 @@ function soundisOn() {
     document.getElementById("soundONButton").classList.add("d-block");
     document.getElementById("soundONButton").classList.remove("d-none");
   } catch (error) {
-    console.log("soundisOn function",error)
+    console.log("soundisOn function", error);
   }
 }
 
 function checkSound() {
-  if (localStorage.getItem("muted").toString() == "true") {
-    console.log("SOUND IS MUTED !!")
-    soundisOff();
-  } else {
-    console.log("SOUND IS NOT MUTED !!")
-    soundisOn();
+  soundisOn();
 
-    beat = new Audio(beatUrl);
-    beat.play().catch((e) => {
-      console.log("INTERACTION REQUIRED")
+  beat = new Audio(beatUrl);
+  beat
+    .play().then((e) => {
+      console.log("SOUND IS PLAYING !!");
+    })
+    .catch((e) => {
+      console.log("INTERACTION REQUIRED");
       clickEvent = window.addEventListener(
         "click",
         () => {
-          console.log("USER INTERACTED AND SOUND SHOULD PLAY")
+          console.log("USER INTERACTED AND SOUND SHOULD PLAY");
           beat.play();
-          beat.addEventListener('ended', function() {
-            console.log("LOOP HAPPENED")
-            this.currentTime = 0;
-            this.play();
-        }, false);
-          soundisOn()
+          beat.addEventListener(
+            "ended",
+            function () {
+              console.log("LOOP HAPPENED");
+              this.currentTime = 0;
+              this.play();
+            },
+            false
+          );
+          soundisOn();
         },
         { once: true }
       );
     });
-    beat.addEventListener('ended', function() {
-      console.log("LOOP HAPPENED")
+  beat.addEventListener(
+    "ended",
+    function () {
+      console.log("LOOP HAPPENED");
       this.currentTime = 0;
       this.play();
-  }, false);
-  }
+    },
+    false
+  );
 }
 
 function turnOff() {
   try {
     beat.pause();
-  } catch (error) {
-  }
-  localStorage.setItem("muted", true);
+  } catch (error) {}
+
   soundisOff();
 }
 
@@ -67,13 +72,16 @@ function turnOn() {
   try {
     beat = new Audio(beatUrl);
     beat.play();
-    beat.addEventListener('ended', function() {
-      this.currentTime = 0;
-      this.play();
-  }, false);
-  } catch (error) {
-  }
-  localStorage.setItem("muted", false);
+    beat.addEventListener(
+      "ended",
+      function () {
+        this.currentTime = 0;
+        this.play();
+      },
+      false
+    );
+  } catch (error) {}
+
   soundisOn();
 }
 
